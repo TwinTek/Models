b74f7e755f7658a60f1b53e840ee22e0a754d219
Artur Sakhno <artur.sakhno@capgemini.com>
2023-05-06 21:57:23 +0200

    TM-38: Update properties
    Added missed properties
    Get rid of optionality for properties
    Added public init to Folder

--- Sources/Models/Folders+Model.swift
+++ Sources/Models/Folders+Model.swift
@@ -2,13 +2,34 @@ import Foundation
 
 public struct Folders: Codable {
     public let folders: [Folder]
+
+    public init(folders: [Folder]) {
+        self.folders = folders
+    }
 }
 
 public struct Folder: Codable {
     public let id: String
     public let name: String
-    public let mascot: String?
+    public let mascot: String
     public let isShared: Bool
+    public let isCollaborative: Bool
     public let createdAt: Double
     public let lastModified: Double
+
+    public init(id: String,
+                name: String,
+                mascot: String,
+                isShared: Bool,
+                isCollaborative: Bool,
+                createdAt: Double,
+                lastModified: Double) {
+        self.id = id
+        self.name = name
+        self.mascot = mascot
+        self.isShared = isShared
+        self.isCollaborative = isCollaborative
+        self.createdAt = createdAt
+        self.lastModified = lastModified
+    }
 }
