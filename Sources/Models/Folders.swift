edd59a5a4a58143d8177cdc00e339d84a874d9a0
Artur Sakhno <artur.sakhno@capgemini.com>
2023-05-06 23:19:58 +0200

    no message

--- Sources/Models/Folders.swift
+++ Sources/Models/Folders.swift
@@ -14,16 +14,16 @@ public struct Folder: Codable {
     public let mascot: String
     public let isShared: Bool
     public let isCollaborative: Bool
-    public let createdAt: Double
-    public let lastModified: Double
+    public let createdAt: Int
+    public let lastModified: Int
 
     public init(id: String,
                 name: String,
                 mascot: String,
                 isShared: Bool,
                 isCollaborative: Bool,
-                createdAt: Double,
-                lastModified: Double) {
+                createdAt: Int,
+                lastModified: Int) {
         self.id = id
         self.name = name
         self.mascot = mascot
